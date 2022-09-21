563745d731a0f47a073219f1ee763a6d90379f99
name_snrl <Demogorgon-74@ya.ru>
2022-09-21 10:25:48 +0500

    Create flake and add profiles modules to it.

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nvimpager.url = "github:lucc/nvimpager";
+    nvim-nightly.url = "github:nix-community/neovim-nightly-overlay";
+    hw-config = {
+      url = "file:///etc/nixos/hardware-configuration.nix";
+      flake = false;
+    };
+    bash-fzf-completion = {
+      url = "github:lincheney/fzf-tab-completion";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+    let
+      findModules = dir:
+        builtins.concatLists (builtins.attrValues (builtins.mapAttrs
+          (name: type:
+            if type == "regular" then [{
+              name = builtins.elemAt (builtins.match "(.*)\\.nix" name) 0;
+              value = dir + "/${name}";
+            }] else if (builtins.readDir (dir + "/${name}"))
+              ? "default.nix" then [{
+              inherit name;
+              value = dir + "/${name}";
+            }] else
+              findModules (dir + "/${name}"))
+          (builtins.readDir dir)));
+    in
+    {
+
+      nixosProfiles = builtins.listToAttrs (findModules ./profiles);
+
+    };
+}
