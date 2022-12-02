616eed6ae1bc251fe00543ed6ac1921014a59edd
name_snrl <Demogorgon-74@ya.ru>
2022-12-02 12:46:11 +0500

    fish: remove some nix tricks because they have problems - fish `& disown` is broken - completion after `--` is broken

--- profiles/fish.nix
+++ profiles/fish.nix
@@ -10,27 +10,16 @@
       reboot = "read -P 'Are you sure? ' yn; [ $yn = y ] && systemctl reboot";
     };
     interactiveShellInit = with pkgs; ''
-      set -U fish_greeting # disable greeting
       stty -ixon # disable flow control
+      set -U fish_greeting # disable greeting
       ${coreutils}/bin/dircolors -c | source
       ${zoxide}/bin/zoxide init --cmd ji fish | source
 
       # Nix tricks
       function njump;   cd $(string split -f1-4 / (nwhich $argv) | string join /); end
       function nwhich;  ${coreutils}/bin/readlink -f (${which}/bin/which $argv); end
-      function nshell;  ${nix}/bin/nix shell  $argv; end
-      function nbuild;  ${nix}/bin/nix build  $argv; end
-      function nsearch; ${nix}/bin/nix search $argv; end
-      function nedit;   ${nix}/bin/nix edit   $argv; end
-      function nrun;    ${nix}/bin/nix run    $argv; end
-
       complete -c njump   -fa "(__fish_complete_command)"
       complete -c nwhich  -fa "(__fish_complete_command)"
-      complete -c nshell  -fa "(NIX_GET_COMPLETIONS=2 nix shell  self#)"
-      complete -c nbuild  -fa "(NIX_GET_COMPLETIONS=2 nix build  self#)"
-      complete -c nrun    -fa "(NIX_GET_COMPLETIONS=2 nix run    self#)"
-      complete -c nsearch -fa "(NIX_GET_COMPLETIONS=2 nix search self#)"
-      complete -c nedit   -fa "(NIX_GET_COMPLETIONS=2 nix edit   self#)"
     '';
   };
 }
