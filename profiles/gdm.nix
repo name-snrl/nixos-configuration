e371db8b0eca0e8058f2dcaca7f22673a34853b2
name_snrl <Demogorgon-74@ya.ru>
2022-12-09 05:58:26 +0500

    gdm: remove xserver stuff as far as possible

--- profiles/gdm.nix
+++ profiles/gdm.nix
@@ -1,34 +1,36 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-  systemd.services.display-manager.serviceConfig.LogNamespace = "desktop-session";
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+
+  systemd = with lib; {
+    defaultUnit = "graphical.target";
+    services.display-manager = {
+      enable = true;
+      script = "${pkgs.gnome.gdm}/bin/gdm";
+      restartIfChanged = false;
+
+      after = [
+        "acpid.service"
+        "systemd-logind.service"
+        "systemd-user-sessions.service"
+      ];
+
+      environment = config.services.xserver.displayManager.job.environment //
+        optionalAttrs config.hardware.opengl.setLdLibraryPath
+          { LD_LIBRARY_PATH = makeLibraryPath [ pkgs.addOpenGLRunpath.driverLink ]; };
+
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = "200ms";
+        SyslogIdentifier = "display-manager";
+        LogNamespace = "desktop-session";
+      };
+    };
+  };
+
   environment.etc."systemd/journald@desktop-session.conf".text = ''
     [Journal]
     SystemMaxUse=200M
   '';
-  services.xserver = {
-    displayManager = {
-      gdm = {
-        wayland = true;
-        enable = true;
-      };
-    };
-    enable = true;
-    excludePackages = with pkgs; with xorg; [
-      xorgserver.out
-      xrandr
-      xrdb
-      setxkbmap
-      iceauth
-      xlsclients
-      xset
-      xsetroot
-      xinput
-      xprop
-      xauth
-      xf86inputevdev.out
-      xterm
-      xdg-utils
-      nixos-icons
-    ];
-  };
 }
