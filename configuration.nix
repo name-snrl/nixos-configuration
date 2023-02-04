9c8f95071661f26d5cd615cd11dd2338a162ace4
name_snrl <Demogorgon-74@ya.ru>
2023-02-04 22:38:05 +0600

    users: use hashed passwords

--- configuration.nix
+++ configuration.nix
@@ -219,16 +219,25 @@ with lib;
     hardwareClockInLocalTime = true;
   };
 
-  users.users.${config.userName} = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "video"
-      "libvirtd"
-      "docker"
-      "adbusers"
-    ];
+  users = {
+    mutableUsers = false;
+    users = {
+      root.hashedPassword =
+        "$6$68YTRwVh7sUS1onf$VwXU4CSQ9/RbbERzYV8TNtfNM8eraZarUZ4oyxXhXHu1j/4ItbSAhuUkkzfc7FF0XKChZnn.hPisvojMSUfM81";
+      ${config.userName} = {
+        hashedPassword =
+          "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "video"
+          "libvirtd"
+          "docker"
+          "adbusers"
+        ];
+      };
+    };
   };
 
   # work stuff
