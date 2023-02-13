40b539f62338468570273538c590ac850096807d
name_snrl <Demogorgon-74@ya.ru>
2023-02-13 06:07:06 +0600

    profiles: delete adb

--- profiles/adb.nix
+++
@@ -1,4 +0,0 @@
-{
-  programs.adb.enable = true;
-  users.users.default.extraGroups = [ "adbusers" ];
-}
--- profiles/misc.nix
+++ profiles/misc.nix
@@ -8,6 +8,9 @@
   qt.enable = true;
   qt.platformTheme = "qt5ct";
 
+  programs.adb.enable = true;
+  users.users.default.extraGroups = [ "adbusers" ];
+
   services.dbus.implementation = "broker";
   documentation.man.generateCaches = true;
 }
