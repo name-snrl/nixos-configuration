ce940e9a821eb474b59e0cde06ea59f5a6f06d48
name_snrl <Demogorgon-74@ya.ru>
2023-02-19 02:57:10 +0600

    parallelize nix-prefetch-url calls in genWallhaven script

--- overlay/genWallhavenCollection.sh
+++ overlay/genWallhavenCollection.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-fill_json () {
-    urls="$(
-    data="$(curl -s "$@")"
+get_urls () {
+    data="$(curl -s "$*")"
 
     echo "$data" | jq -r '.data | map(.path) | .[]'
 
@@ -11,13 +10,23 @@ fill_json () {
         for (( i = 2; i <= last_page; i++ )); do
             curl -s "$*?page=$i" | jq -r '.data | map(.path) | .[]'
         done
-    fi)"
+    fi
+}
 
-    for i in $urls; do
-        hash="$(nix-prefetch-url "$i")"
-        obj='{"url": "'"$i"'", "sha256": "'"$hash"'"}'
-        jq --argjson obj "$obj" '. +[$obj]' wallhaven-collection.json > tmp &&
-            mv tmp wallhaven-collection.json
+get_pairs () {
+    for url in $(get_urls "$*"); do
+        nix-prefetch-url "$url" | sed -E "s#.*#$url=&#" &
+    done
+    wait
+}
+
+fill_json () {
+    for line in $(get_pairs "$*"); do
+        jq --arg url "${line%=*}" \
+            --arg hash "${line#*=}" \
+            '. + [{ url: $url, sha256: $hash }]' \
+            wallhaven-collection.json > tmp-col &&
+            mv tmp-col wallhaven-collection.json
     done
 }
 
@@ -25,4 +34,3 @@ echo '[]' > wallhaven-collection.json
 
 fill_json "https://wallhaven.cc/api/v1/collections/snrl/1046186"
 fill_json "https://wallhaven.cc/api/v1/collections/snrl/1046197"
-
