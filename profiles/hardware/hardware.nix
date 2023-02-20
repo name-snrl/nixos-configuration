63e17a1575ce8956634ef75e4cbc36665047f72a
name_snrl <Demogorgon-74@ya.ru>
2023-02-20 20:17:53 +0600

    fix system.activationScripts.trackpoint

--- profiles/hardware/hardware.nix
+++ profiles/hardware/hardware.nix
@@ -1,4 +1,4 @@
-{
+{ config, lib, ... }: {
   # TODO depends on host spec
   hardware.enableRedistributableFirmware = true;
   zramSwap.enable = true;
@@ -10,4 +10,14 @@
     sensitivity = 130;
     speed = 180;
   };
+  # TODO create pr for this fix
+  # trigger devices recursively
+  system.activationScripts.trackpoint =
+    let
+      udevadm = "${config.systemd.package}/bin/udevadm";
+      cfg = config.hardware.trackpoint;
+    in
+    lib.mkForce ''
+      ${udevadm} trigger "$(${udevadm} trigger -v --dry-run --attr-match=name="${cfg.device}")"
+    '';
 }
