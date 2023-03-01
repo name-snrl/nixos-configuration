34c6f41230fd6be8fe4e5d026b5e0d4bb1028671
name_snrl <Demogorgon-74@ya.ru>
2023-03-01 17:49:36 +0600

    tor: stop using bridges

--- modules/system/networking/tor.nix
+++ modules/system/networking/tor.nix
@@ -7,11 +7,6 @@
       settings = {
         ExitNodes = "{ua}, {nl}, {gb}";
         ExcludeNodes = "{ru},{by},{kz}";
-        UseBridges = true;
-        ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/obfs4proxy";
-        Bridge = ''
-          obfs4 89.58.38.13:5372 0EAD0F94886C75A6322FCA2F5ADC5C51AC18CA3B cert=HnXzqW/GHDRbqhi2qpTM1X86eIRCOKgeh3AYGvJB+aNfL9UzA4/PVyYRXjmU0BEVhcV8TQ iat-mode=0
-        '';
       };
     };
     privoxy.enable = true;
