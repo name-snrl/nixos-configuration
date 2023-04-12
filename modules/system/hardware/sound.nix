0dcee16782aa2ebcd2f8786153efdcca17cff57e
name_snrl <Demogorgon-74@ya.ru>
2023-04-12 15:26:20 +0600

    sound: remove config.pipewire (option have been removed)

--- modules/system/hardware/sound.nix
+++ modules/system/hardware/sound.nix
@@ -3,11 +3,6 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-    config.pipewire = {
-      "context.properties" = {
-        "default.clock.allowed-rates" = [ 44100 48000 96000 ];
-      };
-    };
   };
   security.rtkit.enable = true;
 }
