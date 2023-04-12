96b66eed824af0c7a2a0fc55a7143865b79c1e95
name_snrl <Demogorgon-74@ya.ru>
2023-04-13 04:35:24 +0600

    dns: override ngrok-free.app to localhost

--- modules/system/networking/dnsmasq.nix
+++ modules/system/networking/dnsmasq.nix
@@ -1,6 +1,11 @@
 {
   services.dnsmasq = {
     enable = true;
-    settings.address = [ "/eu.ngrok.io/127.0.0.1" "/eu.ngrok.io/::1" ];
+    settings.address = [
+      "/eu.ngrok.io/127.0.0.1"
+      "/eu.ngrok.io/::1"
+      "/ngrok-free.app/127.0.0.1"
+      "/ngrok-free.app/::1"
+    ];
   };
 }
