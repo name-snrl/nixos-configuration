1e56022cd881c93df71fd12fe609b8484eac1348
name_snrl <Demogorgon-74@ya.ru>
2023-04-20 17:48:58 +0500

    update time zone

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
   i18n.defaultLocale = "en_GB.UTF-8";
   time = {
-    timeZone = "Asia/Almaty";
+    timeZone = "Asia/Yekaterinburg";
     hardwareClockInLocalTime = true;
   };
 
