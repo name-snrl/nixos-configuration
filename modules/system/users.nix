62ce1f72df216abca15360699a3ed8ef9c3926a0
name_snrl <Demogorgon-74@ya.ru>
2023-05-14 14:11:46 +0500

    users: add default user to dialout group

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -6,7 +6,7 @@
       hashedPassword =
         "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "dialout" ];
     };
     root.hashedPassword =
       "$6$68YTRwVh7sUS1onf$VwXU4CSQ9/RbbERzYV8TNtfNM8eraZarUZ4oyxXhXHu1j/4ItbSAhuUkkzfc7FF0XKChZnn.hPisvojMSUfM81";
