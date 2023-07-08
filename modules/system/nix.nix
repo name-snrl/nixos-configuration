66aa75583c1a8da5ae171dbf41933cc7a232a8f4
name_snrl <Demogorgon-74@ya.ru>
2023-07-08 11:29:34 +0500

    nix: use the module to add shlyupa's binary cache

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,4 +1,6 @@
 { config, pkgs, inputs, ... }: {
+  imports = [ inputs.shlyupa.nixosModules.cache ];
+  nur.ilya-fedin.cache.enable = true;
   nix = {
     package = pkgs.nixUnstable;
 
@@ -15,11 +17,9 @@
       trusted-users = [ "root" "@wheel" config.users.users.default.name ];
       substituters = [
         "https://nix-community.cachix.org"
-        "https://ilya-fedin.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "ilya-fedin.cachix.org-1:QveU24a5ePPMh82mAFSxLk1P+w97pRxqe9rh+MJqlag="
       ];
     };
 
