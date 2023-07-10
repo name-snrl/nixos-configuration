00e66a4a778a348a9a2023ff15e182f7513176ee
name_snrl <Demogorgon-74@ya.ru>
2023-07-10 17:19:03 +0500

    xdg-portals: use kde dialogs

--- lib.nix
+++ lib.nix
@@ -3,7 +3,12 @@ rec {
   forAllSystems = genAttrs systems.flakeExposed;
 
   pkgsFor = system: import inputs.nixpkgs {
-    overlays = [ inputs.self.overlay ];
+    overlays = [
+      inputs.self.overlay
+      # TODO remove overlay and add portal.conf when a version with this PR
+      # https://github.com/flatpak/xdg-desktop-portal/pull/955 appears in nixpkgs
+      inputs.shlyupa.overlays.portal
+    ];
     localSystem = { inherit system; };
     config = {
       allowUnfree = true;
