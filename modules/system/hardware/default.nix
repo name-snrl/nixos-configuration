6fbb5cef542e5fd5212223575d93907014c0e3d2
name_snrl <Demogorgon-74@ya.ru>
2023-07-20 16:13:42 +0500

    hardware: add `TODO`

--- modules/system/hardware/default.nix
+++ modules/system/hardware/default.nix
@@ -10,7 +10,8 @@
     sensitivity = 150;
     speed = 250;
   };
-  # temporarly fix https://github.com/systemd/systemd/issues/28345
+  # temporarly fix of https://github.com/systemd/systemd/issues/28345
+  # TODO create PR in nixpkgs with fix trackpoint module
   services.udev.extraRules = with config.hardware.trackpoint; ''
     ACTION=="add|change", SUBSYSTEM=="input", ENV{ID_INPUT_POINTINGSTICK}=="1", ENV{ID_BUS}=="i8042", ATTR{device/device/speed}="${toString speed}", ATTR{device/device/sensitivity}="${toString sensitivity}"
   '';
