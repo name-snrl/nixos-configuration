d444c7ae66ceb6418a5cb9cf5d21aecfe9bbad33
name_snrl <Demogorgon-74@ya.ru>
2023-08-02 15:59:14 +0500

    work: add `ck-client` service

--- modules/system/desktop/work.nix
+++ modules/system/desktop/work.nix
@@ -1,10 +1,22 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   security.pki.certificateFiles = [
-    (__fetchurl {
-      url =
-        "file:///home/${config.users.users.default.name}/nixos-configuration/tawasalca.crt";
-      sha256 = "sha256-XokEwCbkPf747dxgw79ZXIs6tB2EW35R9ZaPA6Jd4S4=";
+    (pkgs.fetchurl {
+      url = "file:///home/${config.users.users.default.name}/nixos-configuration/tawasalca.crt";
+      hash = "sha256-XokEwCbkPf747dxgw79ZXIs6tB2EW35R9ZaPA6Jd4S4=";
     })
   ];
   programs.openvpn3.enable = true;
+  systemd.user.services.ck-client =
+    let
+      ck-client = pkgs.fetchurl {
+        url = "https://github.com/cbeuw/Cloak/releases/download/v2.7.0/ck-client-linux-amd64-v2.7.0";
+        hash = "sha256-lP8KzK+FuqHpAuH8xKjYI3/AU7EbPDENYq4IEIsINk0=";
+        executable = true;
+      };
+    in
+    {
+      wantedBy = [ "sway-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      script = "${ck-client} -u -c ~/.openvpn/ckclient.json";
+    };
 }
