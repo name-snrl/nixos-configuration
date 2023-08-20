9433e38c2a30c3cf3f6fb4a164818a34e9720321
name_snrl <Demogorgon-74@ya.ru>
2023-08-20 14:10:08 +0500

    mime: use neovim as default application for text/plain

--- modules/home/applications/mime.nix
+++ modules/home/applications/mime.nix
@@ -1,6 +1,7 @@
 {
   xdg.mime.defaultApplications = {
     "application/pdf" = "sioyek.desktop";
+    "text/plain" = "nvim.desktop";
     "image/jpeg" = "swayimg.desktop";
     "image/png" = "swayimg.desktop";
   };
