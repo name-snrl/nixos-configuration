1967bd535a2b420a360b9b245723970c20129417
name_snrl <Demogorgon-74@ya.ru>
2023-08-28 00:04:41 +0500

    mime: use sioyek as default app for fb2

--- modules/home/applications/mime.nix
+++ modules/home/applications/mime.nix
@@ -1,5 +1,6 @@
 {
   xdg.mime.defaultApplications = {
+    "application/x-fictionbook+xml" = "sioyek.desktop";
     "application/pdf" = "sioyek.desktop";
     "text/plain" = "nvim.desktop";
     "image/jpeg" = "swayimg.desktop";
