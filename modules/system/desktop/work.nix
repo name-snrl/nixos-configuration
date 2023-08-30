6cf58bea192378d6c14423f1628025551170aa72
name_snrl <Demogorgon-74@ya.ru>
2023-08-30 19:52:22 +0500

    work: return to the builtin fetchurl for downloading local files

--- modules/system/desktop/work.nix
+++ modules/system/desktop/work.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }: {
   security.pki.certificateFiles = [
-    (pkgs.fetchurl {
+    (builtins.fetchurl {
       url = "file:///home/${config.users.users.default.name}/nixos-configuration/tawasalca.crt";
-      hash = "sha256-XokEwCbkPf747dxgw79ZXIs6tB2EW35R9ZaPA6Jd4S4=";
+      sha256 = "sha256-XokEwCbkPf747dxgw79ZXIs6tB2EW35R9ZaPA6Jd4S4=";
     })
   ];
   programs.openvpn3.enable = true;
