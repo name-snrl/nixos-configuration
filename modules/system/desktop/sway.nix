95a5e0c6643f1a46810437778af2778ff01de692
name_snrl <Demogorgon-74@ya.ru>
2023-10-20 05:38:04 +0500

    sway: fcitx already exist in autostart

--- modules/system/desktop/sway.nix
+++ modules/system/desktop/sway.nix
@@ -56,7 +56,6 @@
       tmpfiles.rules = with pkgs; [
         "L+ %h/.config/autostart/firefox.desktop                 - - - - ${firefox-wayland}/share/applications/firefox.desktop"
         "L+ %h/.config/autostart/org.flameshot.Flameshot.desktop - - - - ${flameshot}/share/applications/org.flameshot.Flameshot.desktop"
-        "L+ %h/.config/autostart/org.fcitx.Fcitx5.desktop        - - - - ${config.i18n.inputMethod.package}/etc/xdg/autostart/org.fcitx.Fcitx5.desktop"
       ];
 
       services.mako = {
