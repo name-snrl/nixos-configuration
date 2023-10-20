300e69dd6d534928ac3dca71293aa25df9430d9b
name_snrl <Demogorgon-74@ya.ru>
2023-10-20 16:19:09 +0500

    sway: replace polkit service with upstream

--- modules/system/desktop/sway.nix
+++ modules/system/desktop/sway.nix
@@ -35,94 +35,96 @@
     dictd.DBs = with pkgs.dictdDBs; [ wiktionary ];
   };
 
-  systemd.user =
-    let
-      serviceConf = {
-        Slice = "session.slice";
-        Restart = "always";
-        RestartSec = 2;
-      };
-    in
-    {
-      targets.sway-session = {
-        description = "sway compositor session";
-        documentation = [ "man:systemd.special(7)" ];
-        bindsTo = [ "graphical-session.target" ];
-        wants = [ "graphical-session-pre.target" "xdg-desktop-autostart.target" ];
-        after = [ "graphical-session-pre.target" ];
-        before = [ "xdg-desktop-autostart.target" ];
-      };
-
-      tmpfiles.rules = with pkgs; [
-        "L+ %h/.config/autostart/firefox.desktop                 - - - - ${firefox-wayland}/share/applications/firefox.desktop"
-        "L+ %h/.config/autostart/org.flameshot.Flameshot.desktop - - - - ${flameshot}/share/applications/org.flameshot.Flameshot.desktop"
-      ];
-
-      services.mako = {
-        description = "Lightweight Wayland notification daemon";
-        documentation = [ "man:mako(1)" ];
-        partOf = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type = "dbus";
-          BusName = "org.freedesktop.Notifications";
-          ExecCondition = ''
-            /bin/sh -c '[ -n "$WAYLAND_DISPLAY" ]'
-          '';
-          ExecStart = "${pkgs.mako}/bin/mako";
-          ExecReload = "${pkgs.mako}/bin/makoctl reload";
+  systemd = {
+    packages = with pkgs; [ polkit-kde-agent ];
+    user =
+      let
+        serviceConf = {
+          Slice = "session.slice";
+          Restart = "always";
+          RestartSec = 2;
         };
-        environment.PATH = lib.mkForce null;
-        wantedBy = [ "sway-session.target" ];
-      };
-
-      services.waybar = {
-        description = "Highly customizable bar for Sway";
-        partOf = [ "graphical-session.target" ];
-        after = [ "graphical-session.target" ];
-        requisite = [ "graphical-session.target" ];
-        script = "${pkgs.waybar}/bin/waybar";
-        serviceConfig = serviceConf // {
-          ExecReload = "kill -SIGUSR2 $MAINPID";
+      in
+      {
+        targets.sway-session = {
+          description = "sway compositor session";
+          documentation = [ "man:systemd.special(7)" ];
+          bindsTo = [ "graphical-session.target" ];
+          wants = [ "graphical-session-pre.target" "xdg-desktop-autostart.target" ];
+          after = [ "graphical-session-pre.target" ];
+          before = [ "xdg-desktop-autostart.target" ];
         };
-        environment.PATH = lib.mkForce null;
-        wantedBy = [ "sway-session.target" ];
-      };
 
-      services.swayidle = {
-        description = "Idle management daemon";
-        partOf = [ "graphical-session.target" ];
-        script = "${pkgs.swayidle}/bin/swayidle -w";
-        serviceConfig = serviceConf;
-        environment.PATH = lib.mkForce null;
-        wantedBy = [ "sway-session.target" ];
-      };
+        tmpfiles.rules = with pkgs; [
+          "L+ %h/.config/autostart/firefox.desktop                 - - - - ${firefox-wayland}/share/applications/firefox.desktop"
+          "L+ %h/.config/autostart/org.flameshot.Flameshot.desktop - - - - ${flameshot}/share/applications/org.flameshot.Flameshot.desktop"
+        ];
 
-      services.polkit-agent = {
-        description = "Polkit authentication agent";
-        script = "${pkgs.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1";
-        serviceConfig = serviceConf;
-        environment.PATH = lib.mkForce null;
-        wantedBy = [ "sway-session.target" ];
-      };
+        services.mako = {
+          description = "Lightweight Wayland notification daemon";
+          documentation = [ "man:mako(1)" ];
+          partOf = [ "graphical-session.target" ];
+          after = [ "graphical-session.target" ];
+          serviceConfig = {
+            Type = "dbus";
+            BusName = "org.freedesktop.Notifications";
+            ExecCondition = ''
+              /bin/sh -c '[ -n "$WAYLAND_DISPLAY" ]'
+            '';
+            ExecStart = "${pkgs.mako}/bin/mako";
+            ExecReload = "${pkgs.mako}/bin/makoctl reload";
+          };
+          environment.PATH = lib.mkForce null;
+          wantedBy = [ "sway-session.target" ];
+        };
 
-      services.autotiling = {
-        description = "Automatically alternates the container layout";
-        partOf = [ "graphical-session.target" ];
-        script = "${pkgs.autotiling-rs}/bin/autotiling-rs";
-        serviceConfig = serviceConf;
-        environment.PATH = lib.mkForce null;
-        wantedBy = [ "sway-session.target" ];
-      };
+        services.waybar = {
+          description = "Highly customizable bar for Sway";
+          partOf = [ "graphical-session.target" ];
+          after = [ "graphical-session.target" ];
+          requisite = [ "graphical-session.target" ];
+          script = "${pkgs.waybar}/bin/waybar";
+          serviceConfig = serviceConf // {
+            ExecReload = "kill -SIGUSR2 $MAINPID";
+          };
+          environment.PATH = lib.mkForce null;
+          wantedBy = [ "sway-session.target" ];
+        };
 
-      services.wl-clip-persist = {
-        description = "Keep clipboard even after programs close";
-        partOf = [ "graphical-session.target" ];
-        script = "${pkgs.wl-clip-persist}/bin/wl-clip-persist -c both";
-        serviceConfig = serviceConf;
-        environment.PATH = lib.mkForce null;
-        wantedBy = [ "sway-session.target" ];
-      };
+        services.swayidle = {
+          description = "Idle management daemon";
+          partOf = [ "graphical-session.target" ];
+          script = "${pkgs.swayidle}/bin/swayidle -w";
+          serviceConfig = serviceConf;
+          environment.PATH = lib.mkForce null;
+          wantedBy = [ "sway-session.target" ];
+        };
 
-    };
+        services.plasma-polkit-agent = {
+          after = [ "graphical-session.target" ];
+          serviceConfig = serviceConf;
+          environment.PATH = lib.mkForce null;
+          wantedBy = [ "sway-session.target" ];
+        };
+
+        services.autotiling = {
+          description = "Automatically alternates the container layout";
+          partOf = [ "graphical-session.target" ];
+          script = "${pkgs.autotiling-rs}/bin/autotiling-rs";
+          serviceConfig = serviceConf;
+          environment.PATH = lib.mkForce null;
+          wantedBy = [ "sway-session.target" ];
+        };
+
+        services.wl-clip-persist = {
+          description = "Keep clipboard even after programs close";
+          partOf = [ "graphical-session.target" ];
+          script = "${pkgs.wl-clip-persist}/bin/wl-clip-persist -c both";
+          serviceConfig = serviceConf;
+          environment.PATH = lib.mkForce null;
+          wantedBy = [ "sway-session.target" ];
+        };
+
+      };
+  };
 }
