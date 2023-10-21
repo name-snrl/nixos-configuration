0aef93779fbdabe18ff8929d904d3a18aeb777ac
name_snrl <Demogorgon-74@ya.ru>
2023-10-22 01:22:38 +0500

    locale: enable automatic-timezoned and geoclue2

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -1,7 +1,6 @@
 {
   i18n.defaultLocale = "en_GB.UTF-8";
-  time = {
-    timeZone = "Asia/Yekaterinburg";
-    hardwareClockInLocalTime = true;
-  };
+  time.hardwareClockInLocalTime = true;
+  location.provider = "geoclue2";
+  services.automatic-timezoned.enable = true;
 }
