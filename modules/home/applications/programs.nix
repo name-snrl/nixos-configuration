a658ed6220de4b831702be9d38f10124a1688ec6
name_snrl <Demogorgon-74@ya.ru>
2023-10-21 13:13:47 +0500

    programs: replace custom direnv module with upstream

--- modules/home/applications/programs.nix
+++ modules/home/applications/programs.nix
@@ -1,5 +1,6 @@
 {
   programs = {
+    direnv.enable = true;
     htop = {
       enable = true;
       settings = {
--- modules/home/direnv.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }: with pkgs; {
-  programs.fish.interactiveShellInit = "${direnv}/bin/direnv hook fish | source";
-  environment.systemPackages = [ direnv ];
-  systemd.user.tmpfiles.rules = [
-    "f+ %h/.config/direnv/direnvrc - - - - source ${nix-direnv.override { nix = config.nix.package; }}/share/nix-direnv/direnvrc"
-  ];
-}
