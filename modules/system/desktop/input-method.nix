3ec7b9a366addbaa7053a68cc28ded930d4fd78f
name_snrl <Demogorgon-74@ya.ru>
2023-10-21 17:52:20 +0500

    input-method: init

---
+++ modules/system/desktop/input-method.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }: {
+  i18n = {
+    inputMethod.enabled = "fcitx5";
+    inputMethod.fcitx5.addons = [ pkgs.fcitx5-mozc ];
+  };
+}
--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -1,12 +1,7 @@
-{ pkgs, ... }: {
+{
   i18n.defaultLocale = "en_GB.UTF-8";
   time = {
     timeZone = "Asia/Yekaterinburg";
     hardwareClockInLocalTime = true;
-  };
-
-  i18n = {
-    inputMethod.enabled = "fcitx5"; # TODO if desktop
-    inputMethod.fcitx5.addons = [ pkgs.fcitx5-mozc ];
   };
 }
