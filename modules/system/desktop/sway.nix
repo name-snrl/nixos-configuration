7cd962b50f91935725cde99482d8e6344dca29dd
name_snrl <Demogorgon-74@ya.ru>
2024-01-27 01:03:42 +0500

    sway: don't need to run the `swaync` service, the dbus service does it

--- modules/system/desktop/sway.nix
+++ modules/system/desktop/sway.nix
@@ -48,7 +48,6 @@
     packages = with pkgs; [
       foot
       polkit-kde-agent
-      swaynotificationcenter
     ];
     user =
       let
@@ -72,8 +71,6 @@
           "L+ %h/.config/autostart/firefox.desktop                 - - - - ${firefox-wayland}/share/applications/firefox.desktop"
           "L+ %h/.config/autostart/org.flameshot.Flameshot.desktop - - - - ${flameshot}/share/applications/org.flameshot.Flameshot.desktop"
         ];
-
-        services.swaync.wantedBy = [ "sway-session.target" ];
 
         services.waybar = {
           description = "Highly customizable bar for Sway";
