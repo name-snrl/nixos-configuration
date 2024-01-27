6f57319ed5e704cea1b406e5f22f012e1588f2d3
name_snrl <Demogorgon-74@ya.ru>
2024-01-27 12:06:35 +0500

    xdg-autostart: manual configuration of all entries

--- modules/profiles/system/desktop/sway/xdg-autostart.nix
+++ modules/profiles/system/desktop/sway/xdg-autostart.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
+  xdg.autostart.enable = lib.mkForce false;
   systemd.user = {
     targets.sway-session = {
       wants = [ "xdg-desktop-autostart.target" ];
@@ -8,6 +9,7 @@
     tmpfiles.rules = with pkgs; [
       "L+ %h/.config/autostart/firefox.desktop                 - - - - ${firefox-wayland}/share/applications/firefox.desktop"
       "L+ %h/.config/autostart/org.flameshot.Flameshot.desktop - - - - ${flameshot}/share/applications/org.flameshot.Flameshot.desktop"
-    ];
+    ] ++ lib.optional (config.i18n.inputMethod.enabled == "fcitx5")
+      "L+ %h/.config/autostart/org.fcitx.Fcitx5.desktop        - - - - ${config.i18n.inputMethod.package}/etc/xdg/autostart/org.fcitx.Fcitx5.desktop";
   };
 }
--- modules/profiles/system/locale.nix
+++ modules/profiles/system/locale.nix
@@ -2,6 +2,5 @@
   i18n.defaultLocale = "en_GB.UTF-8";
   time.hardwareClockInLocalTime = true;
   location.provider = "geoclue2";
-  systemd.user.services."app-geoclue\\x2ddemo\\x2dagent@autostart".enable = false; # TODO https://github.com/NixOS/nixpkgs/pull/262625
   services.automatic-timezoned.enable = true;
 }
