dc7532cb4179caf026cc019dd4531a10369f4795
name_snrl <Demogorgon-74@ya.ru>
2024-02-19 19:41:20 +0500

    git: finish configuring branching workflow
    These changes allow to pull and push from different repos. Also added
    some new aliases to simplify the workflow.

--- modules/profiles/home/git.nix
+++ modules/profiles/home/git.nix
@@ -17,7 +17,6 @@
       };
 
       pager.difftool = true;
-      push.autoSetupRemote = true;
 
       diff = {
         tool = "difftastic";
@@ -28,11 +27,26 @@
         "difftastic".cmd = ''difft "$LOCAL" "$REMOTE"'';
       };
 
+      commit.verbose = true;
+
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+      };
+
+      remote.pushdefault = "origin";
+
       alias = {
         st = "status";
-        cm = "commit -v";
-        cb = "switch -c";
+        cm = "commit";
+        sw = "switch";
         dt = "difftool";
+        fx = "commit --fixup";
+        au = "remote add -f upstream";
+        rb = "rebase --autosquash --autostash --interactive";
+        prb = "pull --rebase --autostash";
+        cb = "switch -c";
+        bd = "branch -D";
         l = "log --graph --pretty='%C(bold yellow)%h%Creset * %C(bold magenta)%an%Creset * %C(blue)%ar%Creset: %s %C(auto)%d%Creset'";
         lg = "log --graph --pretty=format:'%C(bold yellow)%h%Creset * %C(bold magenta)%an%Creset * %C(blue)%ar%Creset *%C(auto)%d%Creset%n%n%B'";
         # no blobs and no checkout, because checkout has been taught to bulk
