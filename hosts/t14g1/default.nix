4478afe361aeb5313dd6de8a7f48b1258ef82848
name_snrl <Demogorgon-74@ya.ru>
2024-03-30 16:01:03 +0500

    t14g1: init

---
+++ hosts/t14g1/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  config,
+  importsFromAttrs,
+  ...
+}:
+{
+  imports =
+    [ ./hw-config.nix ]
+    ++ importsFromAttrs {
+      importByDefault = true;
+      modules = inputs.self.nixosModules;
+      imports = {
+        profiles.system.servers.openssh = false;
+      };
+    };
+
+  #boot.initrd.kernelModules = [ "i915" ]; # Enable early KMS
+
+  # Firmware
+  services.fwupd.enable = true; # https://fwupd.org/lvfs/devices/
+
+  # CPU
+  powerManagement.cpuFreqGovernor = "schedutil";
+
+  # GPU acceleration
+  hardware.opengl.extraPackages = with pkgs; [ intel-media-driver ];
+
+  host-specs = {
+    device-type = "laptop";
+    output-name = "eDP-1";
+    cores = 8;
+    ram = 16;
+    wifi = true;
+    bluetooth = true;
+    battery = true;
+    webcam = true;
+    als = false;
+  };
+
+  system.stateVersion = "24.05";
+}
