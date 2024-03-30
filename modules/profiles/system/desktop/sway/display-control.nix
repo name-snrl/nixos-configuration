517adae86bc475645f7379c48303be4ce58beca4
name_snrl <Demogorgon-74@ya.ru>
2024-03-30 14:58:36 +0500

    display-control: temporarily replace `clight` and `wluma` with `brillo`

--- modules/profiles/system/desktop/sway/display-control.nix
+++ modules/profiles/system/desktop/sway/display-control.nix
@@ -5,40 +5,40 @@
 # environment.systemPackages = [ pkgs.wl-gammarelay-rs ];
 #
 # and then can be completely moved to `sway`.
-{ pkgs, ... }:
-{
+_: {
+  hardware.brillo.enable = true;
   # TODO move module in upstream
   # wluma
-  environment.systemPackages = [ pkgs.wluma ];
-  services.udev.packages = [ pkgs.wluma ];
-  systemd = {
-    packages = [ pkgs.wluma ];
-    user.services.wluma.wantedBy = [ "sway-session.target" ];
-    # TODO remove after version with https://github.com/maximbaz/wluma/pull/93
-    user.services.wluma.serviceConfig.PrivateMounts = false;
-  };
+  #environment.systemPackages = [ pkgs.wluma ];
+  #services.udev.packages = [ pkgs.wluma ];
+  #systemd = {
+  #  packages = [ pkgs.wluma ];
+  #  user.services.wluma.wantedBy = [ "sway-session.target" ];
+  #  # TODO remove after version with https://github.com/maximbaz/wluma/pull/93
+  #  user.services.wluma.serviceConfig.PrivateMounts = false;
+  #};
   users.users.default.extraGroups = [ "video" ];
   # TODO replace with `wl-gammarelay-rs`
   # clight
-  systemd.user.services."app-clight@autostart".enable = false; # TODO https://github.com/NixOS/nixpkgs/pull/262624
-  services.clight = {
-    enable = true;
-    temperature.day = 6500;
-    temperature.night = 5500;
-    settings = {
-      daytime = {
-        sunrise = "7:00";
-        sunset = "23:00";
-      };
-      backlight = {
-        no_auto_calibration = true;
-        no_smooth_transition = true;
-      };
-      inhibit.disabled = true;
-      keyboard.disabled = true;
-      dimmer.disabled = true;
-      dpms.disabled = true;
-      screen.disabled = true;
-    };
-  };
+  #systemd.user.services."app-clight@autostart".enable = false; # TODO https://github.com/NixOS/nixpkgs/pull/262624
+  #services.clight = {
+  #  enable = true;
+  #  temperature.day = 6500;
+  #  temperature.night = 5500;
+  #  settings = {
+  #    daytime = {
+  #      sunrise = "7:00";
+  #      sunset = "23:00";
+  #    };
+  #    backlight = {
+  #      no_auto_calibration = true;
+  #      no_smooth_transition = true;
+  #    };
+  #    inhibit.disabled = true;
+  #    keyboard.disabled = true;
+  #    dimmer.disabled = true;
+  #    dpms.disabled = true;
+  #    screen.disabled = true;
+  #  };
+  #};
 }
