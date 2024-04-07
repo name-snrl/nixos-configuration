dc964bac586fbfd755bf9fde86121eade64dd3d4
name_snrl <Demogorgon-74@ya.ru>
2024-04-07 05:13:12 +0500

    pkgs: move `nvim-full` to a separate file

---
+++ pkgs/nvim-full/default.nix
@@ -0,0 +1,87 @@
+{
+  nvim,
+  vimPlugins,
+  gnumake, # for required telescope-fzf-native.nvim
+  gcc, # for required telescope-fzf-native.nvim
+  curl, # for required translate.nvim
+  fd, # for required telescope.nvim
+  ripgrep, # for required telescope.nvim
+  zoxide, # for required telescope-zoxide
+
+  # languages stuff
+  lua-language-server,
+  stylua,
+  selene,
+
+  nixd,
+  nil,
+  nixfmt-rfc-style,
+  deadnix,
+  statix,
+
+  nodePackages,
+  shfmt,
+  shellcheck,
+
+  mdformat,
+  languagetool-rust,
+
+  coursier,
+  bazel-buildtools,
+}:
+
+nvim.override {
+  repo = "https://github.com/name-snrl/nvim";
+  extraName = "-full";
+  viAlias = true;
+  withPython3 = true;
+  rebuildWithTSParsers = true;
+  extraTSParsers = with vimPlugins.nvim-treesitter-parsers; [
+    luap
+    luadoc
+    nix
+    fish
+    scala
+    java
+    starlark
+    go
+    rust
+    css
+    yaml
+    dockerfile
+  ];
+  extraBinPath = [
+    gnumake
+    gcc
+    curl
+    fd
+    ripgrep
+    zoxide
+
+    lua-language-server
+    stylua
+    selene
+
+    nixd
+    nil
+    nixfmt-rfc-style
+    deadnix
+    statix
+
+    nodePackages.bash-language-server
+    shfmt
+    shellcheck
+
+    (mdformat.withPlugins (
+      p: with p; [
+        mdformat-gfm
+        mdformat-frontmatter
+        mdformat-footnote
+      ]
+    ))
+    languagetool-rust
+
+    coursier
+    bazel-buildtools
+  ];
+}
--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -9,62 +9,7 @@ in
 
   osc = final.callPackage ./osc { };
 
-  nvim-full = final.nvim.override {
-    repo = "https://github.com/name-snrl/nvim";
-    extraName = "-full";
-    viAlias = true;
-    withPython3 = true;
-    rebuildWithTSParsers = true;
-    extraTSParsers = with final.vimPlugins.nvim-treesitter-parsers; [
-      luap
-      luadoc
-      nix
-      fish
-      scala
-      java
-      starlark
-      go
-      rust
-      css
-      yaml
-      dockerfile
-    ];
-    extraBinPath = with final; [
-      gnumake # for required telescope-fzf-native.nvim
-      gcc # for required telescope-fzf-native.nvim
-      curl # for required translate.nvim
-      fd # for required telescope.nvim
-      ripgrep # for required telescope.nvim
-      zoxide # for required telescope-zoxide
-
-      # languages stuff
-      lua-language-server
-      stylua
-      selene
-
-      nixd
-      nil
-      nixfmt-rfc-style
-      deadnix
-      statix
-
-      nodePackages.bash-language-server
-      shfmt
-      shellcheck
-
-      (mdformat.withPlugins (
-        p: with p; [
-          mdformat-gfm
-          mdformat-frontmatter
-          mdformat-footnote
-        ]
-      ))
-      languagetool-rust
-
-      coursier
-      bazel-buildtools
-    ];
-  };
+  nvim-full = final.callPackage ./nvim-full { };
 
   writeSymlinkBin =
     pkg: name:
