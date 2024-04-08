48c3e537a97942748b1873870ea79b935efa4583
name_snrl <Demogorgon-74@ya.ru>
2024-04-08 19:01:59 +0500

    input-method: update cfg

--- modules/profiles/system/desktop/input-method.nix
+++ modules/profiles/system/desktop/input-method.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:
 {
-  i18n = {
-    inputMethod.enabled = "fcitx5";
-    inputMethod.fcitx5.addons = [ pkgs.fcitx5-mozc ];
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = [ pkgs.fcitx5-mozc ];
   };
+  programs.sway.extraSessionCommands = ''
+    export GTK_IM_MODULE=fcitx # TODO remove on sway 1.9+
+    export QT_IM_MODULE=fcitx
+  '';
 }
