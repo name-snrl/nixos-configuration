ac81376f4adbab4588fd7868f48911a4f204b70b
name_snrl <Demogorgon-74@ya.ru>
2024-04-09 06:00:02 +0500

    sway(xdg-autostart): install/run all files

--- modules/profiles/system/desktop/sway/xdg-autostart.nix
+++ modules/profiles/system/desktop/sway/xdg-autostart.nix
@@ -1,24 +1,13 @@
+{ pkgs, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  xdg.autostart.enable = lib.mkForce false;
   systemd.user = {
     targets.sway-session = {
       wants = [ "xdg-desktop-autostart.target" ];
       before = [ "xdg-desktop-autostart.target" ];
     };
 
-    tmpfiles.rules =
-      with pkgs;
-      [
-        "L+ %h/.config/autostart/firefox.desktop - - - - ${firefox-wayland}/share/applications/firefox.desktop"
-      ]
-      ++
-        lib.optional (config.i18n.inputMethod.enabled == "fcitx5")
-          "L+ %h/.config/autostart/org.fcitx.Fcitx5.desktop        - - - - ${config.i18n.inputMethod.package}/etc/xdg/autostart/org.fcitx.Fcitx5.desktop";
+    tmpfiles.users.default.rules = with pkgs; [
+      "L+ %h/.config/autostart/firefox.desktop - - - - ${firefox-wayland}/share/applications/firefox.desktop"
+    ];
   };
 }
