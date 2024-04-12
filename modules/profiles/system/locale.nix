0e4fa01ce6e6ef2679ea1a1b2f88241eead80d74
name_snrl <Demogorgon-74@ya.ru>
2024-04-12 20:37:50 +0500

    locale: make default locale non-overridable

--- modules/profiles/system/locale.nix
+++ modules/profiles/system/locale.nix
@@ -1,5 +1,38 @@
+{ config, lib, ... }:
 {
-  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    supportedLocales =
+      with lib;
+      unique (
+        builtins.map
+          (
+            l:
+            (replaceStrings
+              [
+                "utf8"
+                "utf-8"
+                "UTF8"
+              ]
+              [
+                "UTF-8"
+                "UTF-8"
+                "UTF-8"
+              ]
+              l
+            )
+            + "/UTF-8"
+          )
+          (
+            [
+              "C.UTF-8"
+              "en_US.UTF-8"
+              config.i18n.defaultLocale
+            ]
+            ++ (attrValues (filterAttrs (n: _v: n != "LANGUAGE") config.i18n.extraLocaleSettings))
+          )
+      );
+  };
   time.hardwareClockInLocalTime = true;
   location.provider = "geoclue2";
   services.automatic-timezoned.enable = true;
