229b03a0c1d1e27a83a52a2923c03564eedd3eb1
name_snrl <Demogorgon-74@ya.ru>
2024-04-12 20:11:09 +0500

    gf: add locales

--- modules/profiles/system/desktop/gf.nix
+++ modules/profiles/system/desktop/gf.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  i18n.supportedLocales = [
+    "ja_JP.UTF-8/UTF-8"
+    "ru_RU.UTF-8/UTF-8"
+  ];
   users.users.Elizabeth = {
     hashedPassword = "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
     isNormalUser = true;
