4af634d1f1afbd0c034abbb577410b0f5e1099af
name_snrl <Demogorgon-74@ya.ru>
2024-04-12 20:07:27 +0500

    sway(qt): make it apply only on sway

--- modules/profiles/system/desktop/sway/qt.nix
+++ modules/profiles/system/desktop/sway/qt.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 {
-  qt = {
-    enable = true;
-    platformTheme = "qt5ct";
-  };
+  qt.enable = true;
   environment = {
     pathsToLink = [ "/share/Kvantum" ];
     systemPackages = with pkgs; [
+      libsForQt5.qt5ct
       libsForQt5.qtstyleplugin-kvantum
+      qt6Packages.qt6ct
       qt6Packages.qtstyleplugin-kvantum
     ];
   };
+  programs.sway.extraSessionCommands = ''
+    export QT_QPA_PLATFORMTHEME=qt5ct
+  '';
   # to unify dialogs
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 }
