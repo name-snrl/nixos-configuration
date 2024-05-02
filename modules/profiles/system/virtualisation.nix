96d94ba6cbfccfeb66bbdd075cb56ebacb4fd4a3
name_snrl <Demogorgon-74@ya.ru>
2024-05-02 13:05:58 +0500

    virtualisation: remove libvirt

--- modules/profiles/system/virtualisation.nix
+++ modules/profiles/system/virtualisation.nix
@@ -1,10 +1,4 @@
 {
-  programs.virt-manager.enable = true;
-  virtualisation.libvirtd = {
-    enable = true;
-    onBoot = "ignore";
-  };
-  users.users.default.extraGroups = [ "libvirtd" ];
   # nixos-containers networking
   networking.nat = {
     enable = true;
