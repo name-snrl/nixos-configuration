768f1ef2d28d6abcedec2193cbb56486dc944b28
name_snrl <Demogorgon-74@ya.ru>
2024-05-05 15:08:23 +0500

    locale: use the default value for `defaultLocale`
    ...instead of a function that does the same thing. This is so that the
    option can be defined multiple times while maintaining the default value

--- modules/profiles/system/locale.nix
+++ modules/profiles/system/locale.nix
@@ -1,37 +1,8 @@
-{ config, lib, ... }:
+{ options, ... }:
 {
   i18n = {
     defaultLocale = "en_GB.UTF-8";
-    supportedLocales =
-      with lib;
-      unique (
-        builtins.map
-          (
-            l:
-            (replaceStrings
-              [
-                "utf8"
-                "utf-8"
-                "UTF8"
-              ]
-              [
-                "UTF-8"
-                "UTF-8"
-                "UTF-8"
-              ]
-              l
-            )
-            + "/UTF-8"
-          )
-          (
-            [
-              "C.UTF-8"
-              "en_US.UTF-8"
-              config.i18n.defaultLocale
-            ]
-            ++ (attrValues (filterAttrs (n: _v: n != "LANGUAGE") config.i18n.extraLocaleSettings))
-          )
-      );
+    supportedLocales = options.i18n.supportedLocales.default;
   };
   time.hardwareClockInLocalTime = true;
   location.provider = "geoclue2";
