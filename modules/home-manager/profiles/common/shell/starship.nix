1a7f688f97779de8db5c4900d738f15b9a4e517a
name_snrl <Demogorgon-74@ya.ru>
2024-05-24 00:10:39 +0500

    fish: back to starship

---
+++ modules/home-manager/profiles/common/shell/starship.nix
@@ -0,0 +1,73 @@
+{ lib, ... }:
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$nix_shell"
+
+        "$line_break"
+        "$shell"
+        "$jobs"
+        "$cmd_duration"
+        "$character"
+      ];
+
+      username = {
+        style_user = "bold blue";
+        show_always = true;
+      };
+
+      hostname.format = "[$hostname](bold green) in ";
+
+      directory = {
+        style = "cyan";
+        truncation_length = 6;
+        truncate_to_repo = false;
+        read_only = " [RO]";
+        truncation_symbol = ".../";
+        repo_root_style = "italic yellow";
+      };
+
+      git_branch = {
+        style = "bold yellow";
+        only_attached = true;
+      };
+
+      git_commit.format = "on [ $hash](bold yellow) ";
+
+      git_state.style = "bold red";
+
+      nix_shell = {
+        format = "inside [$state $name](bold purple) ";
+        heuristic = true;
+        unknown_msg = "nix shell";
+      };
+
+      shell = {
+        style = "white italic bold";
+        bash_indicator = "bash";
+        fish_indicator = "fish";
+        nu_indicator = "nushell";
+        disabled = false;
+      };
+
+      jobs.format = "[+$number](cyan) ";
+
+      cmd_duration.format = "[$duration](yellow) ";
+
+      character = {
+        success_symbol = "[>](bold green)";
+        error_symbol = "[x](bold red)";
+      };
+
+      continuation_prompt = "[-](bold yellow) ";
+    };
+  };
+}
