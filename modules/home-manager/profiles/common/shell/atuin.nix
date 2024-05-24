f7196b003a49a53db6d59980bbd6ac02f73bd102
name_snrl <Demogorgon-74@ya.ru>
2024-05-25 03:50:47 +0500

    atuin: disable up arrow

--- modules/home-manager/profiles/common/shell/atuin.nix
+++ modules/home-manager/profiles/common/shell/atuin.nix
@@ -1,14 +1,17 @@
 {
   programs.atuin = {
     enable = true;
+    flags = [ "--disable-up-arrow" ];
     settings = {
       update_check = false;
       style = "compact";
+      invert = true;
       inline_height = 15;
       show_help = false;
       show_tabs = false;
       enter_accept = true;
       prefers_reduced_motion = true;
+      keys.scroll_exits = false;
     };
   };
 }
