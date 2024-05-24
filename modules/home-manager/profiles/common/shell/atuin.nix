376385c3764ecbd36815a4d07069463c6ca6067d
name_snrl <Demogorgon-74@ya.ru>
2024-05-25 00:56:51 +0500

    atuin: init

---
+++ modules/home-manager/profiles/common/shell/atuin.nix
@@ -0,0 +1,14 @@
+{
+  programs.atuin = {
+    enable = true;
+    settings = {
+      update_check = false;
+      style = "compact";
+      inline_height = 15;
+      show_help = false;
+      show_tabs = false;
+      enter_accept = true;
+      prefers_reduced_motion = true;
+    };
+  };
+}
