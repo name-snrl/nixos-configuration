9bce0f0ac20614db0dd22fc93508eb220d9ed260
name_snrl <Demogorgon-74@ya.ru>
2024-05-28 14:04:28 +0500

    zellij: add theme and alias

--- modules/home-manager/profiles/common/zellij.nix
+++ modules/home-manager/profiles/common/zellij.nix
@@ -1,8 +1,12 @@
 {
+  home.shellAliases.z = "zellij";
   programs.zellij = {
     enable = true;
     # TODO configure me
-    settings.pane_frames = false;
+    settings = {
+      pane_frames = false;
+      theme = "one-half-dark";
+    };
     #enableFishIntegration = true;
   };
 }
