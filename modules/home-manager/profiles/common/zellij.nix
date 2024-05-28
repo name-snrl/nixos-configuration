951e6308f0952e21c63bdf9b444e405ee8f49705
name_snrl <Demogorgon-74@ya.ru>
2024-05-28 20:02:33 +0500

    zellij: remove arrow bindings

--- modules/home-manager/profiles/common/zellij.nix
+++ modules/home-manager/profiles/common/zellij.nix
@@ -58,11 +58,11 @@
             bind "Alt n" { NewPane; }
             bind "Alt i" { MoveTab "Left"; }
             bind "Alt o" { MoveTab "Right"; }
-            bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
-            bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
-            bind "Alt j" "Alt Down" { MoveFocus "Down"; }
-            bind "Alt k" "Alt Up" { MoveFocus "Up"; }
-            bind "Alt =" "Alt +" { Resize "Increase"; }
+            bind "Alt h" { MoveFocusOrTab "Left"; }
+            bind "Alt l" { MoveFocusOrTab "Right"; }
+            bind "Alt j" { MoveFocus "Down"; }
+            bind "Alt k" { MoveFocus "Up"; }
+            bind "Alt =" { Resize "Increase"; }
             bind "Alt -" { Resize "Decrease"; }
             bind "Alt [" { PreviousSwapLayout; }
             bind "Alt ]" { NextSwapLayout; }
