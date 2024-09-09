05569b028af69dbb47d52dcb14386c0b789e6232
name_snrl <Demogorgon-74@ya.ru>
2024-09-10 00:33:51 +0500

    add TODO

--- modules/nixos/profiles/system/locale.nix
+++ modules/nixos/profiles/system/locale.nix
@@ -5,6 +5,8 @@
     supportedLocales = options.i18n.supportedLocales.default;
   };
   time.hardwareClockInLocalTime = true;
+  # TODO geoclue doesn't work
+  # https://github.com/NixOS/nixpkgs/issues/321121
   location.provider = "geoclue2";
   services.automatic-timezoned.enable = true;
 }
