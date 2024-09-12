ef0d5e00c6e175ab895640f4c55e5e04d7d75b80
name_snrl <Demogorgon-74@ya.ru>
2024-09-12 05:37:35 +0500

    auth: remove root password

--- modules/nixos/profiles/system/auth.nix
+++ modules/nixos/profiles/system/auth.nix
@@ -25,17 +25,14 @@
   users = {
     defaultUserShell = pkgs.fish;
     mutableUsers = false;
-    users = {
-      default = {
-        name = "name_snrl";
-        hashedPassword = "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
-        isNormalUser = true;
-        extraGroups = [
-          "wheel"
-          "dialout"
-        ];
-      };
-      root.hashedPassword = "$6$68YTRwVh7sUS1onf$VwXU4CSQ9/RbbERzYV8TNtfNM8eraZarUZ4oyxXhXHu1j/4ItbSAhuUkkzfc7FF0XKChZnn.hPisvojMSUfM81";
+    users.default = {
+      name = "name_snrl";
+      hashedPassword = "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "dialout"
+      ];
     };
   };
 }
