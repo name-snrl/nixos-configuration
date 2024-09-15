5e0549c8cd551984fe99d3a0deee85859d5710f8
name_snrl <Demogorgon-74@ya.ru>
2024-09-15 14:43:53 +0500

    hm: add nixpkgs profile

---
+++ modules/home-manager/profiles/common/nixpkgs.nix
@@ -0,0 +1,7 @@
+/*
+  There is no reason to configure nixpkgs throught home-manager options, because
+  we create our configurations from flake using the
+  `home-manager.lib.homeManagerConfiguration` function, that generates nixpkgs
+  configuration from its pkgs argument.
+*/
+{ }
