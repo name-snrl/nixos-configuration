2c492d027e753b7d9e7c26ca74986886b688f369
name_snrl <Demogorgon-74@ya.ru>
2024-09-15 15:50:32 +0500

    hm: add nix profile

---
+++ modules/home-manager/profiles/common/nix.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  osConfig,
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+{
+  nix = lib.mkIf (osConfig == null) {
+    package = pkgs.lix;
+    settings = {
+      auto-optimise-store = true;
+      use-xdg-base-directories = true;
+      builders-use-substitutes = true;
+      # Prevent Nix from fetching the registry every time
+      flake-registry = null;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      trusted-users = [ config.home.username ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://ilya-fedin.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "ilya-fedin.cachix.org-1:QveU24a5ePPMh82mAFSxLk1P+w97pRxqe9rh+MJqlag="
+      ];
+    };
+    registry = {
+      self.flake = inputs.self;
+      np = {
+        exact = false;
+        to = {
+          type = "github";
+          owner = "NixOS";
+          repo = "nixpkgs";
+          ref = "nixpkgs-unstable";
+        };
+      };
+    };
+  };
+}
