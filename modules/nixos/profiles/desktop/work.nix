4ea72c5c8964e5b5bb8a583b45ae4f092c04e7ba
name_snrl <Demogorgon-74@ya.ru>
2024-09-30 22:10:34 +0500

    work: cloak has been packaged in nixpkgs

--- modules/nixos/profiles/desktop/work.nix
+++ modules/nixos/profiles/desktop/work.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   security.pki.certificateFiles = [
     (builtins.fetchurl {
@@ -10,18 +15,9 @@
   environment.etc."bazel.bazelrc".text = ''
     startup --max_idle_secs=6000
   '';
-  systemd.user.services.ck-client =
-    let
-      ck-version = "2.9.0";
-      ck-client = pkgs.fetchurl {
-        url = "https://github.com/cbeuw/Cloak/releases/download/v${ck-version}/ck-client-linux-amd64-v${ck-version}";
-        hash = "sha256-0u1QK3AOroUj/8im22ALWRlUcWkmntLJlx4uItpHvm8=";
-        executable = true;
-      };
-    in
-    {
-      wantedBy = [ "sway-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      script = "${ck-client} -u -c ~/.openvpn/ckclient.json";
-    };
+  systemd.user.services.ck-client = {
+    wantedBy = [ "sway-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    script = "${lib.getExe' pkgs.cloak-pt "ck-client"} -u -c ~/.openvpn/ckclient.json";
+  };
 }
