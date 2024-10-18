8374abf6d8075c2742230bc20d6eca37e33c6cd3
name_snrl <Demogorgon-74@ya.ru>
2024-10-19 04:38:33 +0500

    nixos: rewrite input method profile

--- modules/nixos/profiles/desktop/input-method.nix
+++ modules/nixos/profiles/desktop/input-method.nix
@@ -1,13 +1,51 @@
+/*
+  I don't like the input-method modules from nixpkgs, so I wrote my own,
+  following the official documentation:
+
+  https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
+
+  - fcitx5 only
+  - first-class wayland support
+  - sway support
+  - environment.sessionVariables instead environment.variables
+  - this module assumes that your DE supports XDG Autostart, so there are no
+  additional services like those created by home-manager
+*/
 { pkgs, ... }:
+let
+  fcitx5-package = with pkgs; qt6Packages.fcitx5-with-addons.override { addons = [ fcitx5-mozc ]; };
+  gtk_cache =
+    v:
+    pkgs.runCommandLocal "gtk${v}-immodule.cache"
+      {
+        buildInputs = [
+          pkgs.${"gtk" + v}
+          fcitx5-package
+        ];
+      }
+      ''
+        mkdir -p $out/etc/gtk-${v}.0/
+        GTK_PATH=${fcitx5-package}/lib/gtk-${v}.0/ gtk-query-immodules-${v}.0 > $out/etc/gtk-${v}.0/immodules.cache
+      '';
+in
 {
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.waylandFrontend = true;
-    fcitx5.addons = [ pkgs.fcitx5-mozc ];
+  environment = {
+    systemPackages = [
+      fcitx5-package
+      (gtk_cache "2")
+      (gtk_cache "3")
+    ];
+    sessionVariables = {
+      XMODIFIERS = "@im=fcitx";
+      QT_PLUGIN_PATH = [ "${fcitx5-package}/${pkgs.qt6.qtbase.qtPluginPrefix}" ];
+      QT_IM_MODULES = "wayland;fcitx;ibus";
+      # not all packages in nixpkgs includes --enable-wayland-ime flag
+      # p.s. sway doesn't support text-input-v1, so this won't work in sway anyway
+      #NIXOS_OZONE_WL = "1";
+    };
   };
+
   programs.sway.extraSessionCommands = ''
-    export GTK_IM_MODULE=fcitx # TODO remove on sway 1.9+
     export QT_IM_MODULE=fcitx
   '';
 }
