00fc8e0af381632ef521ba43d519a9004fef740a
name_snrl <Demogorgon-74@ya.ru>
2024-10-22 22:37:48 +0500

    misc: enable generate man `configuration.nix`
    In fact, disabling this option does not significantly speed up the
    build. If we want to disable man-cache generation we need to disable
    `documentation.man-db`, but in this case we will lose all the `man`
    pages that we use so much

--- modules/nixos/profiles/misc.nix
+++ modules/nixos/profiles/misc.nix
@@ -2,10 +2,7 @@
 {
   services.dbus.implementation = "broker";
 
-  documentation = {
-    man.generateCaches = true; # necessary for programs that search for man pages
-    nixos.enable = false; # I never read, but it takes too long to compile
-  };
+  documentation.man.generateCaches = true; # necessary for programs that search for man pages
 
   # clear out the junk
   programs.nano.enable = false;
