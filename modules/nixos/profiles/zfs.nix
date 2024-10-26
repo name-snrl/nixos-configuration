d6637376fee069f27573e930dd12aac65b8f862d
name_snrl <Demogorgon-74@ya.ru>
2024-10-26 20:18:14 +0500

    zfs: move `impermanence` specific to a separate config

--- modules/nixos/profiles/zfs.nix
+++ modules/nixos/profiles/zfs.nix
@@ -4,103 +4,113 @@
   config,
   ...
 }:
-let
-  ifImpermanence = lib.mkIf (
-    config.environment ? persistence && config.environment.persistence != { }
-  );
-in
-{
-  disko.devices = {
-    disk.disk0 = {
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            type = "EF00";
-            size = "500M";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=077" ];
+lib.mkMerge [
+  {
+    disko.devices = {
+      disk.disk0 = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=077" ];
+              };
             };
-          };
-          root = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "zroot";
+            root = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
             };
           };
         };
       };
-    };
-    zpool.zroot = {
-      type = "zpool";
-      options = {
-        ashift = "12";
-        autotrim = "on";
-      };
-      rootFsOptions = {
-        acltype = "posixacl";
-        canmount = "off";
-        dnodesize = "auto";
-        normalization = "formD";
-        relatime = "on";
-        xattr = "sa";
-        mountpoint = "none";
-        compression = "lz4";
-        encryption = "on";
-        keylocation = "prompt";
-        keyformat = "passphrase";
-        "com.sun:auto-snapshot" = "false";
-      };
-
-      postCreateHook = "zfs snapshot zroot/rootfs@blank";
-
-      # to prevent mounting after multi-user.target, which can lead to mishaps
-      # (creating files before mounting), all datasets are mounted declaratively
-      # via config. to achieve this we set options.mountpoint = "legacy" and
-      # disable zfs-mount.service
-      datasets = {
-        rootfs = {
-          mountpoint = "/";
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
+      zpool.zroot = {
+        type = "zpool";
+        options = {
+          ashift = "12";
+          autotrim = "on";
         };
-        nix = {
-          mountpoint = "/nix";
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
-          options.compression = "zstd";
+        rootFsOptions = {
+          acltype = "posixacl";
+          canmount = "off";
+          dnodesize = "auto";
+          normalization = "formD";
+          relatime = "on";
+          xattr = "sa";
+          mountpoint = "none";
+          compression = "lz4";
+          encryption = "on";
+          keylocation = "prompt";
+          keyformat = "passphrase";
+          "com.sun:auto-snapshot" = "false";
         };
-        persistent = ifImpermanence {
-          mountpoint = "/persistent";
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
+
+        postCreateHook = "zfs snapshot zroot/rootfs@blank";
+
+        # to prevent mounting after multi-user.target, which can lead to mishaps
+        # (creating files before mounting), all datasets are mounted declaratively
+        # via config. to achieve this we set options.mountpoint = "legacy" and
+        # disable zfs-mount.service
+        datasets = {
+          rootfs = {
+            mountpoint = "/";
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+          };
+          nix = {
+            mountpoint = "/nix";
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            options.compression = "zstd";
+          };
         };
       };
     };
-  };
-  fileSystems = ifImpermanence { "/persistent".neededForBoot = true; };
 
-  boot = {
-    initrd.supportedFilesystems.zfs = true;
-    zfs.forceImportRoot = false;
-    zfs.package = lib.mkIf (lib.hasInfix "cachyos" config.boot.kernelPackages.kernel.modDirVersion) pkgs.zfs_cachyos;
-  };
-  services.zfs = {
-    trim.enable = false; # we don't need this since autotrim is enabled for zroot
-    autoScrub = {
+    boot = {
+      initrd.supportedFilesystems.zfs = true;
+      zfs.forceImportRoot = false;
+      zfs.package = lib.mkIf (lib.hasInfix "cachyos" config.boot.kernelPackages.kernel.modDirVersion) pkgs.zfs_cachyos;
+    };
+    services.zfs = {
+      trim.enable = false; # we don't need this since autotrim is enabled for zroot
+      autoScrub = {
+        enable = true;
+        interval = "weekly";
+      };
+    };
+    networking.hostId = with builtins; substring 0 8 (hashString "md5" config.networking.hostName);
+  }
+
+  (lib.mkIf (config.environment ? persistence && config.environment.persistence != { }) {
+    disko.devices.zpool.zroot.datasets = {
+      persistent = {
+        mountpoint = "/persistent";
+        type = "zfs_fs";
+        options.mountpoint = "legacy";
+      };
+      shared = {
+        mountpoint = "/shared";
+        type = "zfs_fs";
+        options.mountpoint = "legacy";
+      };
+    };
+    fileSystems = {
+      "/persistent".neededForBoot = true;
+      "/shared".neededForBoot = true;
+    };
+    chaotic.zfs-impermanence-on-shutdown = {
       enable = true;
-      interval = "weekly";
+      volume = "zroot/rootfs";
+      snapshot = "blank";
     };
-  };
-  chaotic.zfs-impermanence-on-shutdown = ifImpermanence {
-    enable = true;
-    volume = "zroot/rootfs";
-    snapshot = "blank";
-  };
-  networking.hostId = with builtins; substring 0 8 (hashString "md5" config.networking.hostName);
-}
+  })
+]
