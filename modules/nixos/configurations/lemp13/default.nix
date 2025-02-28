b20482d38e3c37dff21e2e20fb8ba54e13daa7e4
name_snrl <Demogorgon-74@ya.ru>
2025-02-28 15:57:10 +0900

    cfgs: add my system76 lemur pro

---
+++ modules/nixos/configurations/lemp13/default.nix
@@ -0,0 +1,34 @@
+{ inputs, pkgs, ... }:
+{
+  imports = inputs.self.moduleTree.nixos {
+    configurations = false;
+    profiles = {
+      legacy = false;
+      servers = false;
+    };
+  };
+
+  disko.devices.disk.disk0.device = "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_2TB_S7KHNU0X911162F";
+
+  hardware.system76 = {
+    enableAll = true;
+    power-daemon.enable = false;
+  };
+
+  # Firmware
+  # system76 support said that they have made some fixes to the microcode
+  # so disable microcode updates
+  hardware.cpu.intel.updateMicrocode = false;
+
+  # CPU
+  nix.settings.cores = 6;
+
+  # GPU acceleration
+  hardware.graphics.extraPackages = [ pkgs.intel-media-driver ];
+
+  # RAM-specific
+  boot.tmp.useTmpfs = true;
+  zramSwap.memoryPercent = 30;
+
+  system.stateVersion = "25.05";
+}
