0e1ae4498cf74a919daf501d58be7e139da2d45f
name_snrl <Demogorgon-74@ya.ru>
2025-03-06 17:52:46 +0900

    sound: disable autoswitch to headset profile

--- modules/nixos/profiles/desktop/hardware/sound.nix
+++ modules/nixos/profiles/desktop/hardware/sound.nix
@@ -3,6 +3,11 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+    wireplumber.extraConfig.system = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+    };
   };
   security.rtkit.enable = true;
 }
