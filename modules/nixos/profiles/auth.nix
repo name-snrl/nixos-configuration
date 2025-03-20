5d696250f975284282b00b574e6e29ddc8f3d24c
name_snrl <Demogorgon-74@ya.ru>
2025-03-20 16:38:13 +0900

    auth: remove explicit definition of uid (no needed)

--- modules/nixos/profiles/auth.nix
+++ modules/nixos/profiles/auth.nix
@@ -56,7 +56,6 @@
     defaultUserShell = pkgs.fish;
     mutableUsers = false;
     users.${defaultUserName} = {
-      uid = 1000;
       hashedPassword = "$6$6US0iMDXE1K7wj9g$2/JKHfX4VfNETELdt4dTlTUzlmZAmvP4XfRNB5ORVPYNmi6.A4EWpSXkpx/5PrPx1J/LaA41n2NDss/R0Utqh/";
       isNormalUser = true;
       extraGroups = [
