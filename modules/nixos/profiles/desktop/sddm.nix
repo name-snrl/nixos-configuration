e2775e28d4a1167196c9975ad317a7b2bfa772b8
name_snrl <Demogorgon-74@ya.ru>
2025-05-05 16:46:05 +0900

    sddm: move part to legacy

--- modules/nixos/profiles/desktop/sddm.nix
+++ modules/nixos/profiles/desktop/sddm.nix
@@ -1,26 +1,7 @@
-{ lib, pkgs, ... }:
 {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     wayland.compositor = "kwin";
-    theme = "where_is_my_sddm_theme";
-    package = lib.mkDefault pkgs.kdePackages.sddm;
   };
-  environment.systemPackages =
-    let
-      bg = "#2e3440";
-    in
-    [
-      # TODO take color from config
-      (pkgs.where-is-my-sddm-theme.override {
-        variants = [ "qt6" ];
-        themeConfig.General = {
-          background = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          backgroundFill = bg;
-          backgroundMode = "none";
-          blurRadius = 0;
-        };
-      })
-    ];
 }
