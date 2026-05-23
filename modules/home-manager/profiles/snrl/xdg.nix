bf19dd540c6e313fd24991302a49dc1d179d455c
name_snrl <Demogorgon-74@ya.ru>
2026-05-24 05:11:21 +0900

    xdg: add projects and sort

--- modules/home-manager/profiles/snrl/xdg.nix
+++ modules/home-manager/profiles/snrl/xdg.nix
@@ -5,12 +5,13 @@
       enable = true;
       setSessionVariables = true;
       desktop = "${config.home.homeDirectory}/${vars.users.master.dirs.desktop}";
-      publicShare = "${config.home.homeDirectory}/${vars.users.master.dirs.forks}";
-      templates = "${config.home.homeDirectory}/${vars.users.master.dirs.experiments}";
-      download = "${config.home.homeDirectory}/${vars.users.master.dirs.downloads}";
       documents = "${config.home.homeDirectory}/${vars.users.master.dirs.downloads}";
+      download = "${config.home.homeDirectory}/${vars.users.master.dirs.downloads}";
       music = "${config.home.homeDirectory}/${vars.users.master.dirs.downloads}";
       pictures = "${config.home.homeDirectory}/${vars.users.master.dirs.downloads}";
+      projects = "${config.home.homeDirectory}/${vars.users.master.dirs.forks}";
+      publicShare = "${config.home.homeDirectory}/${vars.users.master.dirs.forks}";
+      templates = "${config.home.homeDirectory}/${vars.users.master.dirs.experiments}";
       videos = "${config.home.homeDirectory}/${vars.users.master.dirs.downloads}";
     };
     mimeApps = {
