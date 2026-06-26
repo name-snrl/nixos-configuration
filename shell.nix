38151502af41064820295f119dfab5c5e0d4c1f6
name_snrl <Demogorgon-74@ya.ru>
2026-06-26 13:32:48 +0900

    shell: refine

--- shell.nix
+++ shell.nix
@@ -7,22 +7,23 @@
   perSystem =
     { pkgs, config, ... }:
     {
-      devShells.default = config.pre-commit.devShell.overrideAttrs (oa: {
-        nativeBuildInputs = oa.nativeBuildInputs or [ ] ++ (with pkgs; [ bashInteractive ]);
-      });
+      devShells.default = config.pre-commit.devShell;
 
-      pre-commit.settings.hooks = {
-        treefmt = {
-          enable = true;
-          package = config.treefmt.build.wrapper;
-        };
-        statix = {
-          enable = true; # check. not everything can be fixed, but we need to know what
-          settings.format = "stderr";
-          settings.config =
-            ((pkgs.formats.toml { }).generate "statix.toml" {
-              disabled = config.treefmt.programs.statix.disabled-lints;
-            }).outPath;
+      pre-commit.settings = {
+        enabledPackages = with pkgs; [ bashInteractive ];
+        hooks = {
+          treefmt = {
+            enable = true;
+            package = config.treefmt.build.wrapper;
+          };
+          statix = {
+            enable = true; # check. not everything can be fixed, but we need to know what
+            settings.format = "stderr";
+            settings.config =
+              ((pkgs.formats.toml { }).generate "statix.toml" {
+                disabled = config.treefmt.programs.statix.disabled-lints;
+              }).outPath;
+          };
         };
       };
 
@@ -36,10 +37,18 @@
             disabled-lints = [ "repeated_keys" ];
           };
 
-          shellcheck.enable = true;
+          shellcheck = {
+            enable = true;
+            includes = [
+              "install"
+            ];
+          };
           shfmt = {
             enable = true;
             indent_size = 4;
+            includes = [
+              "install"
+            ];
           };
 
           prettier = {
@@ -49,22 +58,7 @@
           };
         };
         settings.formatter = {
-          shellcheck = {
-            includes = [
-              "pkgs/scripts/*"
-              "install"
-            ];
-            excludes = [
-              "*.nix"
-              "*.envrc"
-            ];
-          };
           shfmt = {
-            includes = [
-              "pkgs/scripts/*"
-              "install"
-            ];
-            excludes = [ "*.nix" ];
             options = [ "--case-indent" ];
           };
         };
