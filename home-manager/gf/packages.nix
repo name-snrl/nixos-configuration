8fac1a5ca19c22f1d142ba6243c3ec5a1171db20
name_snrl <Demogorgon-74@ya.ru>
2026-07-07 20:29:15 +0900

    gf: install packages

--- home-manager/gf/packages.nix
+++ home-manager/gf/packages.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }:
 {
+  programs = {
+    firefox = {
+      enable = true;
+      policies.ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          default_area = "navbar";
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
+        };
+      };
+    };
+  };
   home.packages = with pkgs; [
+    haruna
+    kdePackages.elisa
+
     telegram-desktop
     #discord
     zoom-us
