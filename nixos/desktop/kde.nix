e52314f3c3bcdff8fa5e2a1d2af71757cf765e5e
name_snrl <Demogorgon-74@ya.ru>
2026-07-07 19:09:28 +0900

    kde: exclude some packages

--- nixos/desktop/kde.nix
+++ nixos/desktop/kde.nix
@@ -1,6 +1,16 @@
+{ pkgs, ... }:
 {
   services = {
     desktopManager.plasma6.enable = true;
     displayManager.plasma-login-manager.enable = true;
   };
+  # shift the burden of shaping the user environment to the user
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    elisa
+    kate
+    ktexteditor # provides elevated actions for kate
+    krdp
+  ];
 }
